4402225a57b02ca55a089ed40993f529c765ff11
alieftour <alieftour@gmail.com>
2021-10-12 21:51:06 +0700

    menambahkan fungsi pencarian

--- kuliah/pertemuan11/index.php
+++ kuliah/pertemuan11/index.php
@@ -1,6 +1,11 @@
 <?php
 require 'functions.php';
 $mahasiswa = query("SELECT * FROM mahasiswa");
+
+// ketika tombol cari di klik
+if (isset($_POST['cari'])) {
+  $mahasiswa = cari($_POST['keyword']);
+}
 ?>
 
 <!DOCTYPE html>
@@ -19,6 +24,12 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
   <a href="tambah.php">Tambah Data Mahasiswa</a>
   <br><br>
 
+  <form action="" method="POST">
+    <input type="text" name="keyword" size="40" placeholder="masukkan keyword pencarian" autocomplete="off" autofocus>
+    <button type="submit" name="cari">Cari</button>
+  </form>
+  <br>
+
   <table border="1" cellpadding="10" cellspacing="0">
     <tr>
       <th>#</th>
@@ -27,6 +38,13 @@ $mahasiswa = query("SELECT * FROM mahasiswa");
       <th>Aksi</th>
     </tr>
 
+    <?php if (empty($mahasiswa)) : ?>
+      <tr>
+        <td colspan="4">
+          <p style="color: red;">data mahasiswa tidak ditemukan!</p>
+        </td>
+      </tr>
+    <?php endif; ?>
 
     <?php $i = 1;
     foreach ($mahasiswa as $m) : ?>
